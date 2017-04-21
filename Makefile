79bcbcd5628a23363ac4bb8f59c7619685f3fdb1
William Granados <me@wgma00.me>
2017-04-21 15:57:40 -0400

    made changes to bfs in subgraph generation

---
+++ Makefile
@@ -0,0 +1,4 @@
+FLAGS = -Wall -std=c++11
+
+all:
+	g++ ${FLAGS} sim.cpp -o sim
--- sim.cpp
+++ sim.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define MAXBITS 12
+#define len(x) (int)(x).size()
 using namespace std;
 
 void usage(){
@@ -7,6 +8,16 @@ void usage(){
     printf("\tN - number to create a superset on in base 10 and greater than 0\n");
     printf("\tK - maximum number of neighbours each element can have\n");
     printf("\tLOG - optional, print the game to standard out, pass in as yes\n");
+}
+
+bool differ_by_one_bit(const bitset<MAXBITS>&a, const bitset<MAXBITS>&b){
+    int cnt = 0;
+    for(int i = 0;i < MAXBITS;i++){
+        if(a[i] != b[i]){
+            cnt++;
+        }
+    }
+    return cnt == 1;
 }
 
 int get_nk(char **argv, int *N, int *K){
@@ -40,8 +51,40 @@ void simulate(int N, int K){
         q.pop();
         for(auto it: st){
             bitset<MAXBITS>node = it.first;
-            vector< bitset<MAXBITS> >sub_graph = it.second;
-
+            vector< bitset<MAXBITS> >edges = it.second;
+            // check if the flipped node is already in our 
+            for(int i = 0;i < N;i++){
+                node[i] = ~node[i];
+                if(!st.count(node)){
+                    // determine all nodes which differ by one bit with
+                    // new node we're going to add and determine if once
+                    // this node is added if the graph is still maximal K
+                    bool valid = true;
+                    for(auto jt:st){
+                        bitset<MAXBITS>node2 = jt.first;
+                        if(differ_by_one_bit(node, node2)
+                                && len(st[node]) +1 >= K ){
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if(valid){
+                        // now add edges for nodes which differ by one bti
+                        // with the new node we're adding to the graph
+                        State st_cpy = st;
+                        st_cpy[node] = vector< bitset<MAXBITS> >();
+                        for(auto jt:st_cpy){
+                            bitset<MAXBITS>node2 = jt.first;
+                            if(differ_by_one_bit(node, node2)){
+                                st_cpy[node].push_back(node2);
+                                st_cpy[node2].push_back(node);
+                            }
+                        }
+                        q.push(st_cpy);
+                    }
+                }
+                node[i] = ~node[i];
+            }
         }
     } 
 }
